c59aa588f6d1042012c41941e28df7e2eec4ae4a
talopl <taltal2905@gmail.com>
2020-07-13 13:13:11 +0300

    created the jokes app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const BASE_URL = "https://api.chucknorris.io/jokes/random";
+
+  const initialJokeValue = "Loading . . . ";
+
+  const [joke, setJoke] = useState(() => initialJokeValue);
+
+  function fetchJoke() {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => setJoke((value) => data["value"]));
+  }
+
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
+  function changeJoke() {
+    setJoke(initialJokeValue);
+    fetchJoke();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="joke-container">
+        <div className="title">The Funniest Joke Ever</div>
+        <div className="joke" dangerouslySetInnerHTML={{ __html: joke }}></div>
+        <button className="generate-joke-btn" onClick={changeJoke}>
+          New Joke
+        </button>
+      </div>
+    </>
   );
 }
 
